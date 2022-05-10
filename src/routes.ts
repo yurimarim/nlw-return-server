51704537d107f9d98428cedfdc539f289995fe48
yurimarim <yurimarim@hotmail.com>
2022-05-10 11:31:14 -0300

    Feedback Project backend

---
+++ src/routes.ts
@@ -0,0 +1,55 @@
+import { prisma } from './prisma'
+import express from 'express'
+import nodemailer from 'nodemailer'
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
+
+export const routes = express.Router()
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "df16eac0ad9276",
+    pass: "3f1ae9838c1d37"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository) 
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot
+  })
+  // await transport.sendMail({
+  //   from: 'Equipe Feedget <oi@feedget.com>',
+  //   to: 'Yuri Marim <yurimarim12@gmail.com>',
+  //   subject: 'Novo feedback',
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`
+  //   ].join('\n')
+  // })
+
+
+  return res.status(201).send()
+})
+
+  /* 
+    ******************* SHORT SYNTAX *******************
+
+    prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot
+      }
+    })
+  */
